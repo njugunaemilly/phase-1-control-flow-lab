9f3759a9e70625a1cfb272dd56fe4a02fb55fde2
Emilly Wanjiku <emilly.wanjiku@student.moringaschool.com>
2022-11-25 22:10:10 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+const distance = 199;
+
+function scuberGreetingForFeet(distance){
+  
+if (distance<= 400){
+  return 'This one is on me!';     
+}
+else if (distance <= 2500){
+
+  return 'I will gladly take your thirty bucks.';
+}
+else if (distance > 2500){
+  return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+   // Write your code here!
+
+   return (city==='NYC' ? ("Ok, sounds good.") : ("No go."))
+}
+
+const tip = 'generous';
+
+let cheers;
+
+function switchOnCharmFromTip(tip){  
+      // Write your code here!
+  
+      switch (tip){
+        case 'generous':
+        cheers = 'Thank you so much.';
+        break;
+
+        case 'not as generous':
+        cheers = 'Thank you.'
+        break;
+
+        default:
+          cheers = 'Bye.';
+          break
+      }
+      return cheers;
 }
